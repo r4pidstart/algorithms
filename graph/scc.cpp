a6ee59b433959bd075b2513fb2c804fc928da80c
r4pidstart <r4pidstart@gmail.com>
2022-01-14 23:14:10 +0900

    scc / tarjan dfs

---
+++ graph/scc.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// vector<vector<int> > graph(n+1)
+class SCC
+{
+    private:
+        vector<int> finished, visited;
+        stack<int> stk;
+        int visit_cnt=0;
+
+        int tarjanDFS(int now, const vector<vector<int> >& graph)
+        {
+            if(visited[now]) return visited[now];
+            int low_link=visited[now]=++visit_cnt;
+            stk.push(now);
+
+            for(int next : graph[now])
+                if(!finished[next]) low_link=min(low_link, tarjanDFS(next, graph));
+
+            if(low_link==visited[now])
+            {
+                vector<int> component, possible_route;
+                int idx=sccarr.size();
+                while(true)
+                {
+                    int v=stk.top(); stk.pop();
+                    finished[v]=1;
+                    sccidx[v]=idx;
+                    component.push_back(v);
+                    for(int next : graph[v])
+                        possible_route.push_back(next);
+
+                    if(v==now) break;
+                }
+                sccarr.push_back(component);
+                this->graph.push_back(possible_route);
+            }
+            return low_link;
+        }
+
+    public:
+        vector<vector<int> > sccarr;
+        vector<int> sccidx;
+        vector<vector<int> > graph;
+
+        SCC(const vector<vector<int> >& graph)
+        {
+            const int n=graph.size();
+            finished.assign(n,0), visited.assign(n,0), sccidx.assign(n,-1);
+
+            for(int i=1; i<graph.size(); i++)
+                if(!visited[i]) tarjanDFS(i, graph);
+
+            for(auto& it : this->graph)
+            {
+                for(auto& i : it)
+                    i=sccidx[i];
+                it.resize(unique(it.begin(), it.end())-it.begin());
+            }
+        }
+};
